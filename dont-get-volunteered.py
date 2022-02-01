c45597ac818b47cf9601ee293f8a84494b708d86
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-02-01 21:49:27 +0000

    #2

--- dont-get-volunteered.py
+++ dont-get-volunteered.py
@@ -1,21 +1,12 @@
 from Queue import PriorityQueue
 
-GRID_SIZE = 8
-
-DELTAS = [
-    (2, 1),
-    (2, -1),
-    (-2, 1,),
-    (-2, -1),
-    (1, 2),
-    (1, -2),
-    (-1, 2),
-    (-1, -2)
-]
+DELTAS = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]
 
 def next_positions(src):
-    positions = [src + dx * GRID_SIZE + dy for (dx, dy) in DELTAS]
-    return [p for p in positions if p >= 0 and p <= 63]
+    (x, y) = (src // 8, src % 8)
+    ps = [(x + dx, y + dy) for (dx, dy) in DELTAS]
+    ps = [x * 8 + y for (x, y) in ps if x >= 0 and x < 8 and y >= 0 and y < 8]
+    return ps
 
 def solution(src, dst):
     if src == dst: return 0
