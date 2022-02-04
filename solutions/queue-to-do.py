e979a26bb2e4470e850e183fa5fda1e7f76ba6ef
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-02-04 12:48:10 +0000

    zero has identity of XOR

--- solutions/queue-to-do.py
+++ solutions/queue-to-do.py
@@ -1,9 +1,9 @@
 def solution(start, length):
     id = start
-    checksum = None
+    checksum = 0
     for skip in range(length):
         for i in range(length - skip):
-            checksum = id + i if checksum is None else checksum ^ (id + i)
+            checksum ^= (id + i)
         id += length
 
     return checksum
@@ -11,4 +11,3 @@ def solution(start, length):
 assert(solution(0, 3) == 2)
 assert(solution(17, 4) == 14)
 assert(solution(2, 1) == 2)
-solution(2000000000, 10000000)
