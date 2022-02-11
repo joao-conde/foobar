75cf574b4c82c9a70cfde7d66b0d54515f52bdc0
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-02-11 21:25:59 +0000

    #5 solution

--- solutions/bomb-baby.py
+++ solutions/bomb-baby.py
@@ -1,2 +1,10 @@
-def solution(x, y):
-    pass
+def solution(m, f):
+    m, f, cycles = int(m), int(f), 0
+    while m > 0 and f > 0:
+        steps = max(m, f) // min(m, f)
+        cycles += steps
+        if m > f:
+            m -= f * steps
+        else:
+            f -= m * steps
+    return str(cycles - 1) if m == 1 or f == 1 else "impossible"
