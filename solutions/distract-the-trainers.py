0b3517d72a6c84bd2f0ce070b2b224754abe9c77
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-02-27 15:24:17 +0000

    blossom algorithm

--- solutions/distract-the-trainers.py
+++ solutions/distract-the-trainers.py
@@ -1,29 +1,49 @@
 def solution(bananas):
-    pairs = { i: [] for i, _ in enumerate(bananas) }
-    for i, b1 in enumerate(bananas):
-        for j, b2 in enumerate(bananas):
-            if i == j: continue
-            if loops(b1, b2): pairs[i].append(j)
-
-    matches = [item for item in pairs.items() if len(item[1]) > 0]
-    matches.sort(key = lambda item: len(item[1]))
-
-    cnt = 0
-    while len(matches) > 0:
-        (i, ms) = matches[0]
-        pick = ms[0]
-
-        matches = [(i, [x for x in xs if x != pick]) for (i, xs) in matches[1:]]
-        matches = [(i, xs) for i, xs in matches if len(xs) > 0]
-        matches.sort(key = lambda item: len(item[1]))
-        cnt += 1
-
-    return len(bananas) - cnt
+    graph = generate_graph(bananas)
+    matches = blossom_matching(graph)
+    return len(bananas) - matches
 
 def loops(i, j):
     n = i + j
     while n % 2 == 0: n /= 2
     return (i % n) != 0
+
+def generate_graph(l):
+    graph = {i: [] for i in range(len(l))}
+    for i in range(len(l)):
+        for j in range(len(l)):
+            if i != j and loops(l[i], l[j]):
+                graph[i].append(j)
+                graph[j].append(i)
+    return graph
+
+def blossom_matching(g):
+    matched = 0
+    checks = len(g[max(g, key=lambda key: len(g[key]))])
+    while len(g) > 1 and checks >= 1:
+        init_mw_node = min(g, key=lambda key: len(g[key]))
+        if (len(g[init_mw_node])) < 1 :
+            del g[init_mw_node]
+        else:
+            temp_sec_min = [len(g[g[init_mw_node][0]])+1,1]
+            for node_i in g[init_mw_node]:
+                if len(g[node_i]) < temp_sec_min[0]:
+                    temp_sec_min = [len(g[node_i]), node_i]
+                for check_i in range(len(g[node_i])):
+                    if g[node_i][check_i] == init_mw_node:
+                        del g[node_i][check_i]
+                        break
+            for node_i in g[temp_sec_min[1]]:
+                for check_i in range(len(g[node_i])):
+                    if g[node_i][check_i] == temp_sec_min[1]:
+                        del g[node_i][check_i]
+                        break
+            del g[init_mw_node]
+            del g[temp_sec_min[1]]
+            matched += 2
+        if len(g) > 1:
+            checks = len(g[max(g, key=lambda key: len(g[key]))])
+    return matched
 
 print(solution([1, 1]))
 print(solution([1, 7, 3, 21, 13, 19]))
